1fbe81079cbb4eb27f277622e9704d4273a42a72
Christopher Chianelli <chris.chianelli@gmail.com>
2018-02-02 20:48:33 -0500

    backup-3

--- gwt-jackson/src/main/java/com/github/nmorel/gwtjackson/client/deser/jackson/JacksonDeserializationContext.java
+++ gwt-jackson/src/main/java/com/github/nmorel/gwtjackson/client/deser/jackson/JacksonDeserializationContext.java
@@ -1,6 +1,44 @@
 package com.github.nmorel.gwtjackson.client.deser.jackson;
 
+import com.fasterxml.jackson.annotation.ObjectIdGenerator;
+import com.fasterxml.jackson.annotation.ObjectIdResolver;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.KeyDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.UnresolvedForwardReference;
+import com.fasterxml.jackson.databind.deser.impl.ReadableObjectId;
+import com.fasterxml.jackson.databind.introspect.Annotated;
 
-public class JacksonDeserializationContext {
+public class JacksonDeserializationContext extends DeserializationContext {
+
+    protected JacksonDeserializationContext() {
+        super((DeserializationContext) new ObjectMapper().getDeserializationContext());
+    }
+
+    @Override
+    public ReadableObjectId findObjectId(Object id, ObjectIdGenerator<?> generator, ObjectIdResolver resolver) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void checkUnresolvedObjectId() throws UnresolvedForwardReference {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public JsonDeserializer<Object> deserializerInstance(Annotated annotated, Object deserDef) throws JsonMappingException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public KeyDeserializer keyDeserializerInstance(Annotated annotated, Object deserDef) throws JsonMappingException {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
--- gwt-jackson/src/main/java/com/github/nmorel/gwtjackson/client/deser/jackson/JacksonJsonDeserializer.java
+++ gwt-jackson/src/main/java/com/github/nmorel/gwtjackson/client/deser/jackson/JacksonJsonDeserializer.java
@@ -5,11 +5,18 @@ import com.github.nmorel.gwtjackson.client.JsonDeserializer;
 import com.github.nmorel.gwtjackson.client.JsonDeserializerParameters;
 import com.github.nmorel.gwtjackson.client.stream.JsonReader;
 
-public class JacksonJsonDeserializer<T> extends JsonDeserializer<T> {
+public abstract class JacksonJsonDeserializer<T> extends JsonDeserializer<T> {
+
+    com.fasterxml.jackson.databind.JsonDeserializer<T> jacksonDeserializer;
+
+    public JacksonJsonDeserializer(com.fasterxml.jackson.databind.JsonDeserializer<T> jacksonDeserializer) {
+        this.jacksonDeserializer = jacksonDeserializer;
+    }
 
     @Override
     protected T doDeserialize(JsonReader reader, JsonDeserializationContext ctx, JsonDeserializerParameters params) {
         // TODO Auto-generated method stub
+        jacksonDeserializer.deserialize(new JacksonJsonParser(reader,ctx,params), new JacksonDeserializationContext(ctx,params))
         return null;
     }
 
